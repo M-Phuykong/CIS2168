284b78754e50b9f5b74b4a795596d5673c3910cb
Phuykong <m.phuykong@gmail.com>
2022-09-24 20:14:20 -0400

    complete hw05

--- ps-05/MazeGridPanel.java
+++ ps-05/MazeGridPanel.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Queue;
+import java.util.Random;
 import java.util.Stack;
 
 import javax.swing.JPanel;
@@ -11,16 +14,93 @@ public class MazeGridPanel extends JPanel{
 	private int cols;
 	private Cell[][] maze;
 
-	private int[] dRow = { -1, 1, 0, 0 };
-	private int[] dCol = { 0, 0, 1, -1 };
-
+	// top,right,down,left
+	private int[] xDir = {-1,0,1,0};
+	private int[] yDir = {0,1,0,-1};
 
 	// extra credit
 	public void genDFSMaze() {
-		boolean[][] visited;
+		boolean[][] visited = new boolean[rows][cols];
 		Stack<Cell> stack  = new Stack<Cell>();
+		Random random = new Random();
+
 		Cell start = maze[0][0];
 		stack.push(start);
+		visited[0][0] = true;
+
+		while (!stack.isEmpty()){
+
+			Cell curLocation = stack.pop();
+			
+			ArrayList<Integer> possible = new ArrayList<Integer>();
+			
+			if (curLocation.northWall && curLocation.row - 1 >= 0 &&!visited[curLocation.row - 1][curLocation.col]){ possible.add(0); }
+			if (curLocation.eastWall && curLocation.col + 1 < cols && !visited[curLocation.row][curLocation.col + 1]){ possible.add(1); }
+			if (curLocation.southWall && curLocation.row + 1 < rows && !visited[curLocation.row + 1][curLocation.col]){ possible.add(2); }
+			if (curLocation.westWall && curLocation.col - 1 >= 0 && !visited[curLocation.row][curLocation.col - 1]){ possible.add(3); }
+
+			if (possible.size() > 0){
+				stack.push(curLocation);
+				int dir = possible.get(random.nextInt(possible.size()));
+
+				Cell newLoc = maze[curLocation.row + xDir[dir]][curLocation.col + yDir[dir]];
+
+				if (dir == 0){
+					curLocation.northWall = false;
+					newLoc.southWall = false;
+				}
+				else if (dir == 1){
+					curLocation.eastWall = false;
+					newLoc.westWall = false;
+				}
+				else if (dir == 2){
+					curLocation.southWall = false;
+					newLoc.northWall = false;
+				}
+				else {
+					curLocation.westWall = false;
+					newLoc.eastWall = false;
+				}
+
+				visited[newLoc.row][newLoc.col] = true;
+				stack.push(newLoc);
+			}
+			maze[curLocation.row][curLocation.col].repaint();
+		}
+		this.repaint();
+
+	}
+	
+	public void genNWMaze() {
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+
+				if (row == 0 && col == 0) {
+					continue;
+				}
+
+				else if (row == 0) {
+					maze[row][col].westWall = false;
+					maze[row][col - 1].eastWall = false;
+				} else if (col == 0) {
+					maze[row][col].northWall = false;
+					maze[row - 1][col].southWall = false;
+				} else {
+					boolean north = Math.random() < 0.5;
+					if (north) {
+						maze[row][col].northWall = false;
+						maze[row - 1][col].southWall = false;
+					} else { // remove west
+						maze[row][col].westWall = false;
+						maze[row][col - 1].eastWall = false;
+					}
+					maze[row][col].repaint();
+				}
+			}
+		}
+		this.repaint();
+
 	}
 
 	//homework
@@ -32,13 +112,6 @@ public class MazeGridPanel extends JPanel{
 		finish.setBackground(Color.RED);
 		stack.push(start);
 		
-		// Cell curLocation = maze[rows-1][0];
-		// System.out.println(curLocation.row);
-		// System.out.println(curLocation.col);
-		// System.out.println(curLocation.northWall);
-		// System.out.println(curLocation.southWall);
-		// System.out.println(curLocation.eastWall);
-		// System.out.println(curLocation.westWall);
 		while (!stack.isEmpty()){
 
 			Cell curLocation = stack.peek();
@@ -50,27 +123,21 @@ public class MazeGridPanel extends JPanel{
 			curLocation.setBackground(Color.YELLOW);
 
 			if (!curLocation.northWall && !visited(curLocation.row - 1, curLocation.col)){
-
 				Cell toAdd = maze[curLocation.row - 1][curLocation.col];
 				stack.push(toAdd);
+
 			}
 			else if (!curLocation.southWall && !visited(curLocation.row + 1, curLocation.col)) {
-				// curLocation.setBackground(Color.YELLOW);
-
 				Cell toAdd = maze[curLocation.row + 1][curLocation.col];
 				stack.push(toAdd);
 
 			}
 			else if (!curLocation.eastWall && !visited(curLocation.row, curLocation.col + 1)) {
-				// curLocation.setBackground(Color.YELLOW);
-
 				Cell toAdd = maze[curLocation.row][curLocation.col + 1];
 				stack.push(toAdd);
 
 			}
 			else if (!curLocation.westWall && !visited(curLocation.row, curLocation.col - 1)){
-				// curLocation.setBackground(Color.YELLOW);
-
 				Cell toAdd = maze[curLocation.row][curLocation.col - 1];
 				stack.push(toAdd);
 			}
@@ -83,7 +150,6 @@ public class MazeGridPanel extends JPanel{
 		}
 
 		start.setBackground(Color.GREEN);
-
 	}
 
 
@@ -102,37 +168,7 @@ public class MazeGridPanel extends JPanel{
 	}
 
 
-	public void genNWMaze() {
-		
-		for(int row = 0; row  < rows; row++) {
-			for(int col = 0; col < cols; col++) {
 
-				if(row == 0 && col ==0) {
-					continue;
-				}
-
-				else if(row ==0) {
-					maze[row][col].westWall = false;
-					maze[row][col-1].eastWall = false;
-				} else if(col == 0) {
-					maze[row][col].northWall = false;
-					maze[row-1][col].southWall = false;
-				}else {
-					boolean north = Math.random()  < 0.5;
-					if(north) {
-						maze[row][col].northWall = false;
-						maze[row-1][col].southWall = false;
-					} else {  // remove west
-						maze[row][col].westWall = false;
-						maze[row][col-1].eastWall = false;
-					}
-					maze[row][col].repaint();
-				}
-			}
-		}
-		this.repaint();
-		
-	}
 
 	public MazeGridPanel(int rows, int cols) {
 		this.setPreferredSize(new Dimension(800,800));
@@ -151,7 +187,8 @@ public class MazeGridPanel extends JPanel{
 		}
 
 
-		this.genNWMaze();
+		// this.genNWMaze();
+		this.genDFSMaze();
 		this.solveMaze();
 		
 	}
