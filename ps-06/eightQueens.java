12fef1bd19100fb9777ab7d87caecc6a83d790ba
Phuykong <m.phuykong@gmail.com>
2022-09-27 17:18:19 -0400

    start hw-06

---
+++ ps-06/eightQueens.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class eightQueens {
+    
+    int[][] board = {   {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0}    };
+
+    // HashSet<int> posDiag;
+    // HashSet<int> negDiag;
+
+    public eightQueens() {
+
+    }
+    
+
+    private void solve(int position){
+        System.out.println("solving...");
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
