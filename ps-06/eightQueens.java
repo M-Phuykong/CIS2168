33b6f4bc9f2ed40915524a96028b72c9870d38db
Phuykong-SP6 <m.phuykong@gmail.com>
2022-09-28 13:43:19 -0400

    progress

--- ps-06/eightQueens.java
+++ ps-06/eightQueens.java
@@ -2,6 +2,8 @@ import java.util.HashSet;
 
 public class eightQueens {
     
+    final static int N = 8;
+
     int[][] board = {   {0, 0, 0, 0, 0, 0, 0, 0}, 
                         {0, 0, 0, 0, 0, 0, 0, 0}, 
                         {0, 0, 0, 0, 0, 0, 0, 0}, 
@@ -11,32 +13,71 @@ public class eightQueens {
                         {0, 0, 0, 0, 0, 0, 0, 0},
                         {0, 0, 0, 0, 0, 0, 0, 0}   };
 
-    HashSet<Integer> column;
-    HashSet<Integer> posDiag;
-    HashSet<Integer> negDiag;
+
+
 
     public eightQueens() {
 
     }
     
 
-    private void solve(int position){
+    private void solve(){
         System.out.println("solving...");
+        HashSet<Integer> column = new HashSet<Integer>();
+        HashSet<Integer> posDiag = new HashSet<Integer>();
+        HashSet<Integer> negDiag = new HashSet<Integer>();
+        backtrack(0, column, posDiag, negDiag);
+
+
+
     }
 
 
-    private void backtrack(int r, HashSet posDiag, HashSet negDiag){
+    private boolean backtrack(int r, HashSet<Integer> column, HashSet<Integer> posDiag, HashSet<Integer> negDiag ){
+        // System.out.println(column);
 
-        if (r == 8){
-            System.out.println("do something");
+        if (r == N){
+            // System.out.println(board.toString());
+            System.out.println("-----START-----");
+            for (int i = 0; i < board.length; i++) {
+                for (int index = 0; index < board.length; index++) {
+                    System.out.print(board[i][index]);
+                }
+                System.out.println();
+            }
+            System.out.println("-----END----");
+            return true;
         }
 
+        for (int c = 0; c < N; c++){
+
+            if (!column.contains(c) && !posDiag.contains(r + c) && !negDiag.contains(r-c)){
+
+                this.board[r][c] = 1;
+                column.add(c);
+                posDiag.add(r + c);
+                negDiag.add(r - c);
+
+
+                backtrack(r + 1, column, posDiag, negDiag);
+
+                this.board[r][c] = 0;
+                column.remove(c);
+                posDiag.remove(r + c);
+                negDiag.remove(r - c);
+            }
+        }
+
+        return false;
 
     }
 
 
     public static void main(String[] args) {
-        
+        eightQueens EQ = new eightQueens();
+
+        EQ.solve();
+
     }
 
 
