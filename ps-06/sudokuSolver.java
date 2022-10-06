432df1dab509c5f5ee8bd0cce8e6f7b1d7d0ec05
Phuykong <m.phuykong@gmail.com>
2022-10-05 20:56:54 -0400

    sovled.

--- ps-06/sudokuSolver.java
+++ ps-06/sudokuSolver.java
@@ -1,98 +1,16 @@
+import java.io.*;
 import java.util.*;
 
 public class sudokuSolver {
     
     final static int N = 9;
-
-    int[][] board = {   {0, 0, 3, 0, 2, 0, 6, 0, 0}, 
-                        {9, 0, 0, 3, 0, 5, 0, 0, 1}, 
-                        {0, 0, 1, 8, 0, 6, 4, 0, 0}, 
-                        {0, 0, 8, 1, 0, 2, 9, 0, 0}, 
-                        {7, 0, 0, 0, 0, 0, 0, 0, 8}, 
-                        {0, 0, 6, 7, 0, 8, 2, 0, 0}, 
-                        {0, 0, 2, 6, 0, 9, 5, 0, 0},
-                        {8, 0, 0, 2, 0, 3, 0, 0, 9},
-                        {0, 0, 5, 0, 1, 0, 3, 0, 0}   };
-
+    
     public sudokuSolver() {}
 
-// https://stackoverflow.com/questions/15797446/how-do-i-add-values-to-a-set-inside-a-map
     private boolean solve(int[][] board, int pos){
-        /*if(col == 9) {
-            row++;
-            col = 0;
-            //return solve(board, row +1, 0);
-        }
-        if(row== 9) {
-            return true;
-        }*/
+        
+        // end of the board 
         if(pos == 81){
-            return true;
-        }
-
-        int row = pos / 9;
-        int col = pos % 9;
-
-        if(board[row][col] != 0 ) {
-            return solve(board, pos +1);
-        }
-        
-
-        return false;        
-    }
-
-
-
-private void solve() {
-        System.out.println("solving...");
-        HashMap<Integer, HashSet<Integer>> gridMap = new HashMap<Integer, HashSet<Integer>>();
-        HashMap<Integer, HashSet<Integer>> rowMap = new HashMap<Integer, HashSet<Integer>>();
-        HashMap<Integer, HashSet<Integer>> colMap = new HashMap<Integer, HashSet<Integer>>();
-        int remainingZero = 0;
-
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board.length; c++) {
-
-                int gridIndex = (c / 3) + (r / 3) * 3;
-                gridMap.computeIfAbsent(gridIndex, k -> new HashSet<Integer>()).add(board[r][c]);
-                rowMap.computeIfAbsent(r, k -> new HashSet<Integer>()).add(board[r][c]);
-                colMap.computeIfAbsent(c, k -> new HashSet<Integer>()).add(board[r][c]);
-                if (board[r][c] == 0){
-                    remainingZero++;
-                }
-                // if (board[r][c] != 0){
-                // } else{
-                //     remainingZero++;
-                // }
-                
-            }
-        }
-
-        // for (int r = 0; r < board.length; r++){
-        //     for (int c = 0; c < board.length; c++){
-        //         backtrack(r, c, remainingZero, gridMap, rowMap, colMap);
-        //     }
-        // }
-        backtrack(0, 0, remainingZero, gridMap, rowMap, colMap);
-
-        
-        // System.out.println(gridMap.keySet());
-        // System.out.println(rowMap.keySet());
-        // System.out.println(colMap.keySet());
-        // System.out.println(colMap.get(1));
-        // System.out.println(remainingZero);
-    }
-
-    private boolean backtrack(int r, int c,
-                            int remainingZero,
-                            HashMap<Integer, HashSet<Integer>> gridMap, 
-                            HashMap<Integer, HashSet<Integer>> rowMap, 
-                            HashMap<Integer, HashSet<Integer>> colMap) {
-        
-        if (r == N || r < 0 || c == N || c < 0 || board[r][c] != 0){
-            return false;
-        }
-        if (remainingZero == 0){
             for (int i = 0; i < board.length; i++) {
                 for (int index = 0; index < board.length; index++) {
                     System.out.print(board[i][index]);
@@ -101,64 +19,106 @@ private void solve() {
             }
             return true;
         }
-        int gridIndex = (c / 3) + (r / 3) * 3;
 
+        // convert 1d array to 2d index
+        int row = pos / 9;
+        int col = pos % 9;
+
+        // move to the next position
+        if(board[row][col] != 0 ) {
+            return solve(board, pos + 1);
+        }
+
+        // go through all possible choices
         for (int i = 1; i <= 9; i++){
+            // check if the option is valid
+            if (validRow(board, row, col, i) && 
+                validCol(board, row, col, i) && 
+                validBox(board, row, col, i)) {
 
-            if (!gridMap.get(gridIndex).contains(i) && 
-                !rowMap.get(r).contains(i) &&
-                !colMap.get(c).contains(i)){
-                    
-                    board[r][c] = i;
-                    add(gridMap, gridIndex, i);
-                    add(rowMap, r, i);
-                    add(colMap, c, i);
-                    remainingZero--;
-                    // System.out.println(r + " " + c + " " + i);
-        
-                    if (backtrack(r + 1, c, remainingZero, gridMap, rowMap, colMap)){
-                        return true;
-                    }
-                    if (backtrack(r - 1, c, remainingZero, gridMap, rowMap, colMap)){
-                        return true;
-                    };
-                    if (backtrack(r, c + 1, remainingZero, gridMap, rowMap, colMap)) {
-                        return true;
-                    }
-                    if (backtrack(r, c - 1, remainingZero, gridMap, rowMap, colMap)){
-                        return true;
-                    }
-                    
-                    board[r][c] = 0;
-                    remainingZero++;
-                    remove(gridMap, gridIndex, i);
-                    remove(rowMap, r, i);
-                    remove(colMap, c, i);
+                board[row][col] = i;
+                solve(board, pos + 1);
+                // backtrack and reset board to the original state
+                board[row][col] = 0;
             }
 
         }
         
         return false;
-    } 
+    }
 
-    public void add(HashMap<Integer, HashSet<Integer>> map, Integer key, Integer value) {
-        Set<Integer> set = map.get(key);
-        if (set == null) {
-            map.put(key, (HashSet<Integer>) (set = new HashSet<Integer>()));
+    private boolean validRow(int[][] board, int r, int c, int i){
+        for (int j = 0; j < board.length; j++) {
+            if (board[r][j] == i) {
+                return false;
+            }
         }
-        set.add(value);
+        return true;
     }
 
-    public void remove(HashMap<Integer, HashSet<Integer>> map, Integer key, Integer value) {
-        Set<Integer> set = map.get(key);
-        set.remove(value);
+    private boolean validCol(int[][] board, int r, int c, int i) {
+        for (int j = 0; j < board.length; j++) {
+            if (board[j][c] == i) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static void main(String[] args) {
+    private boolean validBox(int[][] board, int r, int c, int i) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                if (board[(r - r % 3) + j][(c - c % 3) + k] == i){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        
         sudokuSolver SS = new sudokuSolver();
-        SS.solve();
+
+        File sudokuTextFile = new File("sudoku.txt");
+        PrintStream out = new PrintStream(new File("sudokuAnswer.txt"));
+        Scanner sc = new Scanner(sudokuTextFile);
+
+        int[][] board = new int[9][9];
+        int arrayInd = 0;
+
+        // redirecting it to a file
+        //
+        System.setOut(out);
+        
+        while (sc.hasNextLine()){
+            String line = sc.nextLine();
+
+            if (line.contains("Grid")){
+                
+                System.out.println(line);
+
+                arrayInd = 0;
+                for (int index = 0; index < board.length; index++) {
+                    for (int inner = 0; inner < board[index].length; inner++) {
+                        board[index][inner] = 0;
+                    }
+                }
+            }
+            else{
+                
+                int[] row = new int[9];
+                for (int i = 0; i < board.length; i++) {
+                    row[i] = Character.getNumericValue(line.charAt(i));
+                }
+                board[arrayInd] = row;
+                arrayInd++;
+            }
+
+            if (arrayInd == 9){
+                SS.solve(board, 0);
+            }
+        }
+        sc.close();
     }
-
-
-
 }
