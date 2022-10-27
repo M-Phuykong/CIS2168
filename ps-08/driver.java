dbffd93de0d0389707bf2d54d00f4df9bb18860d
Phuykong-SP6 <m.phuykong@gmail.com>
2022-10-27 12:52:56 -0400

    progress

--- ps-08/driver.java
+++ ps-08/driver.java
@@ -5,8 +5,9 @@ import java.lang.Math;
 public class driver {
 
     private static int maxWordSetLength = 0;
-    private static Set<String> wordSet = new HashSet<String>();
-    private static Map<String, HashSet<String>> wordMap = new HashMap<String, HashSet<String>>();
+    private static Set<Integer> userGuess = new HashSet<>();
+    private static Map<Integer, List<String>> wordMapByLength = new HashMap<>();
+    private static Map<String, List<String>> wordMap = new HashMap<>();
 
     private static void init(){
         String fileName = "words.txt";
@@ -15,9 +16,18 @@ public class driver {
             Scanner scanner = new Scanner(new File(fileName));
 
             while (scanner.hasNextLine()){
-                String word = scanner.nextLine();
-                wordSet.add(word);
-                maxWordSetLength = Math.max(maxWordSetLength, word.length());
+                String word = scanner.nextLine().toLowerCase();
+                int wordLength = word.length();
+                
+                boolean key = wordMapByLength.containsKey(wordLength);
+                if (!key){
+                    wordMapByLength.put(wordLength, new ArrayList<String>(Arrays.asList(word)));
+                }
+                else{
+                    wordMapByLength.get(wordLength).add(word);
+                }
+
+                maxWordSetLength = Math.max(maxWordSetLength,wordLength);
             }
 
         } catch (FileNotFoundException e){
@@ -25,27 +35,46 @@ public class driver {
         }
     }
 
+    private static Map<String, List<String>> getWordFamilies(List<String> wordList){
+        Map<String, List<String>> wordFamilies = new HashMap<>();
+
+        return wordFamilies;
+    }
+
+    private static String getBestFamily(Map<String, List<String>> wordFamilies){
+        String bestFamily = "";
+        return bestFamily;
+    }
+
+    private static void game(List<String> wordList) {
+        String boardState = "";
+        for (int i = 0; i < wordList.get(0).length(); i++) {
+            boardState += "_";
+        }
+        getWordFamilies(wordList);
+
+
+    }
+
     public static void main(String[] args) {
         
         init();
         
-        int chosenWordLenght;
+        int chosenWordLength;
         Scanner inp = new Scanner(System.in);
 
         System.out.println("-----Welcome to Hangman----");
 
         System.out.println("Please Choose the Length of the Word:");
         do {
-            chosenWordLenght = inp.nextInt();
+            chosenWordLength = inp.nextInt();
         }
-        while (chosenWordLenght > maxWordSetLength);
+        while (chosenWordLength > maxWordSetLength);
 
         System.out.println("How many tries would you like?");
         int numOfTries = inp.nextInt();
 
-        
-        
-
+        game(wordMapByLength.get(chosenWordLength));
 
 
     }
