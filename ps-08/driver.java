e844932aada07006e8a3968d6edc0ada5ed02e48
Phuykong-OfficePC <m.phuykong@gmail.com>
2022-10-27 10:45:08 -0700

    progress

--- ps-08/driver.java
+++ ps-08/driver.java
@@ -5,9 +5,8 @@ import java.lang.Math;
 public class driver {
 
     private static int maxWordSetLength = 0;
-    private static Set<Integer> userGuess = new HashSet<>();
+    private static Set<Character> userGuess = new HashSet<>();
     private static Map<Integer, List<String>> wordMapByLength = new HashMap<>();
-    private static Map<String, List<String>> wordMap = new HashMap<>();
 
     private static void init(){
         String fileName = "words.txt";
@@ -38,6 +37,32 @@ public class driver {
     private static Map<String, List<String>> getWordFamilies(List<String> wordList){
         Map<String, List<String>> wordFamilies = new HashMap<>();
 
+        for (String word : wordList){
+            String curPattern = "";
+
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (userGuess.contains(c)){
+                    curPattern += c;
+                }
+                else{
+                    curPattern += "_";
+                }
+            }
+
+            boolean key = wordFamilies.containsKey(curPattern);
+            if (!key){
+                wordFamilies.put(curPattern, new ArrayList<String>(Arrays.asList(word)));
+            }
+            else {
+                wordFamilies.get(curPattern).add(word);
+            }
+
+        }
+
+        System.out.println(wordFamilies);
+
+
         return wordFamilies;
     }
 
@@ -46,12 +71,59 @@ public class driver {
         return bestFamily;
     }
 
-    private static void game(List<String> wordList) {
+    private static void game() {
+
+        int chosenWordLength;
+        int round = 0;
+        char guess;
         String boardState = "";
-        for (int i = 0; i < wordList.get(0).length(); i++) {
+        List<String> wordList;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("-----Welcome to Hangman----");
+
+        // Get Word Length
+        System.out.println("Please Choose the Length of the Word:");
+        do {
+            chosenWordLength = inp.nextInt();
+        } while (chosenWordLength > maxWordSetLength);
+        
+        // Get number of tries
+        System.out.println("How many tries would you like?");
+        int numOfTries = inp.nextInt();
+        
+        // Initial board state
+        for (int i = 0; i < chosenWordLength; i++) {
             boardState += "_";
         }
-        getWordFamilies(wordList);
+
+        // Initial word list
+        wordList = wordMapByLength.get(chosenWordLength);
+        
+        while (numOfTries > 0){
+            
+            System.out.println("----- " + round + " -----");
+            System.out.println(boardState);
+            
+            do {
+                System.out.println("Please Guess Your Character");
+                guess = inp.next().charAt(0);
+            } while (userGuess.contains(guess));
+            
+            userGuess.add(guess);
+            
+            Map<String, List<String>> wordFamilies = getWordFamilies(wordList);
+            String newState = getBestFamily(wordFamilies);
+            
+            if (!newState.equals(boardState)){
+                boardState = newState;
+            } 
+            else{
+                numOfTries--;
+            }
+            
+            round++;
+        }
 
 
     }
@@ -59,24 +131,7 @@ public class driver {
     public static void main(String[] args) {
         
         init();
-        
-        int chosenWordLength;
-        Scanner inp = new Scanner(System.in);
-
-        System.out.println("-----Welcome to Hangman----");
-
-        System.out.println("Please Choose the Length of the Word:");
-        do {
-            chosenWordLength = inp.nextInt();
-        }
-        while (chosenWordLength > maxWordSetLength);
-
-        System.out.println("How many tries would you like?");
-        int numOfTries = inp.nextInt();
-
-        game(wordMapByLength.get(chosenWordLength));
-
-
+        game();
     }
 
 
