4fa17da4b6ba1560e2cf12fa214e1c7daef8e3f3
Phuykong-OfficePC <m.phuykong@gmail.com>
2022-10-31 07:52:30 -0700

    added retry function

--- ps-08/driver.java
+++ ps-08/driver.java
@@ -80,85 +80,97 @@ public class driver {
 
     private static void game() {
 
-        int chosenWordLength;
-        int round = 0;
-        char guess;
-        String boardState = "";
-        List<String> wordList;
+        String choice;
         Scanner inp = new Scanner(System.in);
         
-        System.out.println("-----Welcome to Hangman----");
-        
-        // Get Word Length
-        System.out.println("Please Choose the Length of the Word:");
         do {
-            chosenWordLength = inp.nextInt();
-        } while (chosenWordLength > maxWordSetLength);
-        
-        // Get number of tries
-        System.out.println("How many tries would you like?");
-        int numOfTries = inp.nextInt();
-        
-        // Initial board state
-        for (int i = 0; i < chosenWordLength; i++) {
-            boardState += "_";
-        }
-        
-        // Initial word list
-        wordList = wordMapByLength.get(chosenWordLength);
-        long boardStateCount = chosenWordLength;
-        
-        while (numOfTries > 0 && boardStateCount != 0){
+            int chosenWordLength;
+            int round = 0;
+            char guess;
+            String boardState = "";
+            List<String> wordList;
+            userGuess.clear();
             
-            System.out.println("----- " + round + " -----");
-            System.out.println("Word: " + boardState + "     " + "[" + wordList.size() + "]");
-            System.out.println("Number of Try Leftover: " + numOfTries);
-            System.out.println("Guessed Letters: " + userGuess.toString());
+            System.out.println("-----Welcome to Hangman----");
             
+            // Get Word Length
+            System.out.println("Please Choose the Length of the Word:");
             do {
-                System.out.println("Please Guess Your Character");
-                guess = inp.next().charAt(0);
-            } while (userGuess.contains(guess));
+                chosenWordLength = inp.nextInt();
+            } while (chosenWordLength > maxWordSetLength || chosenWordLength < 2);
             
-            // add user guess to the universal set
-            userGuess.add(guess);
+            // Get number of tries
+            System.out.println("How many tries would you like?");
+            int numOfTries = inp.nextInt();
             
-            // generate the word families
-            Map<String, List<String>> wordFamilies = getWordFamilies(wordList);
-            
-            // get the best family state
-            String newState = getBestFamily(wordFamilies);
-            
-            // update the word list
-            wordList = wordFamilies.get(newState);
-
-            // count the number of "_" of the new board state
-            boardStateCount = newState.chars().filter(ch -> ch == '_').count();
-            
-            if (!newState.equals(boardState)){
-                boardState = newState;
-            } 
-            else{
-                numOfTries--;
+            // Initial board state
+            for (int i = 0; i < chosenWordLength; i++) {
+                boardState += "_";
             }
             
-            round++;
+            // Initial word list
+            wordList = wordMapByLength.get(chosenWordLength);
+            long boardStateCount = chosenWordLength;
+            
+            while (numOfTries > 0 && boardStateCount != 0){
+                
+                System.out.println("----- " + round + " -----");
+                System.out.println("Word: " + boardState + "     " + "[" + wordList.size() + "]");
+                System.out.println("Number of Try Leftover: " + numOfTries);
+                System.out.println("Guessed Letters: " + userGuess.toString());
+                
+                do {
+                    System.out.println("Please Guess Your Character");
+                    guess = inp.next().charAt(0);
+                    inp.nextLine();
+                } while (userGuess.contains(guess));
+                
+                // add user guess to the universal set
+                userGuess.add(guess);
+                
+                // generate the word families
+                Map<String, List<String>> wordFamilies = getWordFamilies(wordList);
+                
+                // get the best family state
+                String newState = getBestFamily(wordFamilies);
+                
+                // update the word list
+                wordList = wordFamilies.get(newState);
+    
+                // count the number of "_" of the new board state
+                boardStateCount = newState.chars().filter(ch -> ch == '_').count();
+                
+                if (!newState.equals(boardState)){
+                    boardState = newState;
+                } 
+                else{
+                    numOfTries--;
+                }
+                
+                round++;
+                System.out.println();
+            }
+    
+            if (boardStateCount == 0){
+                System.out.println("----- " + round + " -----");
+                System.out.println("Word: " + boardState);
+                System.out.println("Number of Try Leftover: " + numOfTries);
+                System.out.println("Guessed Letters: " + userGuess.toString());
+    
+                System.out.println("You Win!");
+            } else
+            {
+                System.out.println("Sorry, you ran out of guesses.");
+                System.out.println("The Word: " + wordList.get(0));
+            }
+            
             System.out.println();
-        }
+            System.out.println("Would you like to play again? [Y/N]");
+            choice = inp.nextLine().toUpperCase();
 
-        if (boardStateCount == 0){
-            System.out.println("----- " + round + " -----");
-            System.out.println("Word: " + boardState);
-            System.out.println("Number of Try Leftover: " + numOfTries);
-            System.out.println("Guessed Letters: " + userGuess.toString());
-
-            System.out.println("You Win!");
-        } else
-        {
-            System.out.println("Sorry, you ran out of guesses.");
-            System.out.println("The Word: " + wordList.get(0));
-        }
-
+        } while (choice.equals("Y"));
+        
+        inp.close();
     }
 
     public static void main(String[] args) {
