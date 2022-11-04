927af464e6ee480a203ff07c87b2b2d2f28932f0
Phuykong-OfficePC <m.phuykong@gmail.com>
2022-11-04 08:44:13 -0700

    finished insertion sort

--- ps-09/driver.ts
+++ ps-09/driver.ts
@@ -3,8 +3,23 @@
 
 const getRandomInt = (max: number) => Math.floor(Math.random() * max);
 
-function insertionSort(){
+function insertionSort(list: Array<number>): Array<number> {
+    let i: number = 1;
 
+    while (i < list.length) {
+        let j: number = i;
+
+        while (j > 0 && list[j - 1] > list[j]) {
+            let tmp: number = list[j - 1];
+            list[j - 1] = list[j];
+            list[j] = tmp;
+            j--;
+        }
+        
+        i++;
+    }
+
+    return list;
 }
 
 function quickSort(){
@@ -20,14 +35,16 @@ function main(){
     console.log("hello world");
     var endTime = performance.now();
 
-    let list: Array<Number> = [];
+    let list: Array<number> = [];
 
     for (let index = 0; index < 10; index++) {
         list.push(getRandomInt(20));
     }
+
+    console.log(list.toString())
+    console.log(insertionSort(list.slice()).toString());
+    console.log(list.toString())
     
-    console.log(endTime - startTime);
-    console.log(getRandomInt(5));
 }
 
 main()
