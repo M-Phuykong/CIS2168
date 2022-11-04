106b666cc0829a85f4a9a9c12e44b1603c5f2f39
Phuykong <m.phuykong@gmail.com>
2022-11-04 01:54:35 -0400

    started p09

---
+++ ps-09/driver.ts
@@ -0,0 +1,16 @@
+
+
+function 
+
+
+
+
+function main(){
+    var startTime = performance.now();
+    console.log("hello world");
+    var endTime = performance.now();
+    
+    console.log(endTime - startTime);
+}
+
+main()
---
+++ ps-09/test.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class test{
+
+    public static char mostCommonChar(String text){
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c: text.toCharArray()){
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+        int mostCount = 0;
+        char mostLetter = 0;
+
+        for (char c: counts.keySet()){
+            int count = counts.get(c);
+            if (count > mostCount){
+                mostCount = count;
+                mostLetter = c;
+            }
+        }
+
+
+        return mostLetter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mostCommonChar("aabccd"));
+    }
+
+
+
+}
