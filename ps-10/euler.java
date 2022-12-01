ff7e1bc964eea9d9c42758846322728003d5a69f
Phuykong-SP6 <m.phuykong@gmail.com>
2022-12-01 11:24:05 -0500

    update

--- ps-10/euler.java
+++ ps-10/euler.java
@@ -1,33 +1,58 @@
 import java.util.*;
 import java.io.*;
 import edu.uci.ics.jung.graph.Graph;
-import edu.uci.ics.jung.graph.SparseGraph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
 
 public class euler {
 
+    public static void kahn(Graph<Integer, String> graph){
 
+        Stack<Integer> s = new Stack<Integer>();
+        Collection<Integer> vertices;
+        
+        for (int v: vertices){
+            int indegree = graph.inDegree(v);
+            System.out.println(v);
+            System.out.println(new StringBuilder().append("indegree ").append(graph.inDegree(v)).toString());
+            if (indegree == 0){
+                s.push(v);
+                graph.removeVertex(v);
+            }
 
+        }
+
+        System.out.println(s.toString());
+    }
 
     public static void main(String[] args) {
 
         String filename = "keylog.txt";
-        Graph<Integer, String> g = new SparseGraph<Integer, String>(); 
+        Graph<Integer, String> g = new DirectedSparseGraph<Integer, String>(); 
 
         try {
             Scanner scanner = new Scanner(new File(filename));
             
             while (scanner.hasNextLine()){
-                char[] edges = scanner.nextLine().toCharArray();
-
-                // g.addEdge(edges[0] + edges[1], null);
-                System.out.println(new StringBuilder(edges[0]).append(edges[1]), Integer.parseInt(edges[0]), );
+                char[] points = scanner.nextLine().toCharArray();
+                String edge1 = new StringBuilder().append(points[0]).append(points[1]).toString();
+                String edge2 = new StringBuilder().append(points[1]).append(points[2]).toString();
+                
+                g.addEdge(edge1, 
+                        Character.getNumericValue(points[0]), 
+                        Character.getNumericValue(points[1]));
+                
+                g.addEdge(edge2,
+                        Character.getNumericValue(points[1]),
+                        Character.getNumericValue(points[2]));
 
             }
-
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        
+        kahn(g);
         
 
     }
